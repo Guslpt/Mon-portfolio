6cc8815d22df12ec7b8a244b0e6dd9ce58e4a11c
EVDEVZ4E <lepetit.augustin@orange.fr>
2024-01-30 16:28:30 +0100

    test de focntionnalité

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', () => {
         clickCount++;
         if (clickCount === 3) {
-            document.getElementById('styleSheet').setAttribute('href', 'psyStyle.css');
+            const stylesheet = document.getElementById('styleSheet');
+            stylesheet.setAttribute('href', 'psyStyle.css');
+
+            // Revenir à style.css après 10 secondes
+            setTimeout(() => {
+                stylesheet.setAttribute('href', 'style.css');
+            }, 10000); // 10000 millisecondes = 10 secondes
         }
     });
 });
+
